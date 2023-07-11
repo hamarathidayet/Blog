b532541e9a907b5cb0dfac0c14e695a2ba18bd1f
hamarathidayet <112272488+hamarathidayet@users.noreply.github.com>
2023-07-11 13:08:48 +0300

    Responsive-Blog

---
+++ kisisel-blog/js/islem.js
@@ -0,0 +1,48 @@
+$(".sum").click(function(){
+	$("body").css({"background-color":"white","transition":"1s"});
+	$(".sum").css({"color":"#01172d","transition":"1s"});
+	$(".night").css({"color":"#01172d","transition":"1s"});
+	$(".navbar").css({"background-color":"white","transition":"1s"});
+	$(".nav-item").css({"color":"#01172d","transition":"1s","background-color":"white","border-color":"#01172d"});
+	$(".navbar-brand-s").css({"color":"#01172d","transition":"1s"});
+	$(".nice").css({"color":"#01172d","transition":"1s"});
+	$(".nav-link").css({"color":"#01172d","transition":"1s","background-color":"white"});
+	$("form-label").css({"color":"white","transition":"1s"});
+	$(".form-content").css({"background-color":"#01172d","transition":"1s"});
+	$(".h1-tag").css({"color":"#01172d","transition":"1s"});
+	$(".about").css({"color":"white","transition":"1s"});
+	$(".about").css({"background-color":"#01172d","transition":"1s"});
+
+	$(".aciklama").css({"color":"white","transition":"1s"});
+	$(".aciklama").css({"background-color":"#01172d","transition":"1s"});
+	$(".altciz").css({"color":"#01172d","transition":"1s"});
+	$(".altciz").css({"background-color":"white","transition":"1s"});
+
+})
+
+$(".night").click(function(){
+	$("body").css({"background-color":"#01172d","transition":"1s"});
+	$(".sum").css({"color":"white","transition":"1s"});
+	$(".navbar").css({"background-color":"#01172d","transition":"1s"});
+	$(".night").css({"color":"white","transition":"1s"});
+	$(".nav-link").css({"color":"white","transition":"1s","background-color":"#01172d"});
+	
+	$(".nav-item").css({"color":"white","transition":"1s","background-color":"#01172d","border-color":"white"});
+	$(".navbar-brand-s").css({"color":"white","transition":"1s"});
+	$(".nice").css({"color":"white","transition":"1s",});
+	$("form-label").css({"color":"#01172d","transition":"1s"});
+	$(".form-content").css({"background-color":"white","transition":"1s"});
+	$(".h1-tag").css({"color":"white","transition":"1s"});
+
+	$(".about").css({"color":"#01172d","transition":"1s"});
+	$(".about").css({"background-color":"white","transition":"1s"});
+	$(".aciklama").css({"color":"#01172d","transition":"1s"});
+	$(".aciklama").css({"background-color":"white","transition":"1s"});
+
+	$(".altciz").css({"color":"white","transition":"1s"});
+	$(".altciz").css({"background-color":"#01172d","transition":"1s"});
+
+
+
+
+})
